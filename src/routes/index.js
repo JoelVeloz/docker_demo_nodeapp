3002d75ae47475dc7c72c864303f9484bb2a32f5
JoelVeloz <joevpazmq@espol.edu.ec>
2024-01-27 00:54:34 -0500

    updt

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,9 +1,27 @@
 const express = require('express');
+const exec = require('child_process').exec;
+
+// const 
 const router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
+router.get('/', function (req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
+router.get("/curl/:ip", function (req, res) {
+  const ip = req.params.ip;
+  const cmd = `curl ${ip}`;
+  exec(cmd, function (error, stdout, stderr) {
+    if (error) {
+      console.error(`exec error: ${error}`);
+      res.send(`exec error: ${error}`);
+      return;
+    }
+    console.log(`stdout: ${stdout}`);
+    console.log(`stderr: ${stderr}`);
+    res.send(`stdout: ${stdout}`);
+  });
+
+});
 module.exports = router;
